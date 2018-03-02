ddec9050bc55a9562542ba0fa310e719bbb136d9
Winfred Nadeau <winfred@hired.com>
2018-03-02 12:37:50 -0600

    pass class methods through to subclass
    useful for building a "living documentation system" for you descendants
    am doing this at hired/hired to end the days of wondering
    "what does this activity mean?"
    
    because as it turns out, scaling an entire domain of events
    based on naming convention alone is not enough,
    and enforcing the convention is actually really hard for many reasons
    so here is some english to help us all jive better

--- lib/descendants_describable.rb
+++ lib/descendants_describable.rb
@@ -4,6 +4,19 @@ module DescendantsDescribable
   extend ActiveSupport::Concern
 
   module ClassMethods
+
+    #
+    # Boots up your subclass descriptor DSL
+    #
+    # ex-
+    #
+    #   Activity.describe_descendants_with(Activities::Descriptors) do
+    #     type :user_did_thing do             # see `#type` comment below for explanation
+    #       actor_required                    # see `#method_missing` comment below for explanation
+    #     end
+    #   end
+    #
+    #
     def describe_descendants_with(description_module, &block)
       descriptor = DescendantsDescriptor.new(self, description_module, block)
       descriptor.describe_descendants
@@ -55,6 +68,22 @@ module DescendantsDescribable
       self.new_class.send(:include, mod)
     end
 
+
+    #
+    # Within your DSL, creates a new subclass (descendant) of the provided top level class.
+    #
+    # The method is named `type` because the primary use case right now is for setting an
+    #   activities.type column for an Activity < ActiveRecord::Base Single-table Inheritance
+    #   activity/event stream data model.
+    #
+    # Maybe there's a better, less-coupled-to-a-certain-use-case name for this...
+    #  make a PR and pitch it if you think of it. :)
+    #
+    #
+    # ex-
+    #   type :user_did_thing # creates an empty `UserDidThing < Activity` class.
+    #
+    #
     def type(name)
 
       self.new_class = begin
@@ -73,18 +102,62 @@ module DescendantsDescribable
       self.new_class = nil
     end
 
-    def method_missing(method, *args)
+    #
+    # By using method_missing, we can turn module names into snake_case descriptor methods
+    # This is how you turn your descriptor modules into a DSL of your own
+    # so you may declaratively add behaviors to your subclasses.
+    #
+    # It'll also catch class-level methods you define on your superclass and pass the args along.
+    #
+    # ex-
+    #   Adding behavior & static descriptions of the sublcasses to be used when rendering an HTML
+    #   page that serves as 'living documentation' for your application's event stream.
+    #
+    #   type :user_did_thing do
+    #
+    #     # example of including _behavior_, such as ActiveModel validations.
+    #     # logically equivalent to `include Activities::Descriptors::ActorRequired` in subclass
+    #     actor_required
+    #
+    #     # example of calling class method defined in superclass
+    #     # logically equivalent to `description "whatever"` in subclass
+    #     description 'Indicates the user has done a thing'
+    #   end
+    #
+    # If you look closely, you can also use modules to apply 'grouping' to sets of activities
+    #   such that the subclasses defined within that block all get the same top-level behavior
+    #   (see `continue_module_tree`)
+    #
+    # You run the risk of adding too much complexity to your DSL usage if you have
+    # too many levels like this though. Just look at the spec file and how many nested
+    # ducky-things there are... It's technically supported but be highly skeptical of this feature.
+    #
+    def method_missing(method, *args, &block)
       if self.new_class.present?
-        add_module @description_module.const_get(method.to_s.camelize)
+        modify_class(args, method)
       else
-        @common_modules << @description_module.const_get(method.to_s.camelize)
-        yield if block_given?
-        @common_modules.pop
+        continue_module_tree(method, &block)
       end
     end
 
     def respond_to?(method)
       @description_module.const_get(method.to_s.camelize) rescue false
+    end
+
+    private
+
+    def continue_module_tree(method)
+      @common_modules << @description_module.const_get(method.to_s.camelize)
+      yield if block_given?
+      @common_modules.pop
+    end
+
+    def modify_class(args, method)
+      if self.new_class.respond_to?(method)
+        self.new_class.send(method, *args)
+      else
+        add_module @description_module.const_get(method.to_s.camelize)
+      end
     end
   end
 end
